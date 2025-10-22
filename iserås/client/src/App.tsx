21a19eb6e7dd11a54f8776cd0034cc08f023a4b7
erm0es <emelie.rosendahl@hotmail.se>
2025-10-22 19:31:17 +0200

    refactor: move page content from App.tsx into separate page components

--- iserås/client/src/App.tsx
+++ iserås/client/src/App.tsx
@@ -1,40 +1,18 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
-import Hero from "./components/hero/Hero";
 import Footer from "./components/footer/Footer";
-import ServicesSection from "./components/servicesSection/ServicesSection";
-import AboutSection from "./components/aboutSection/AboutSection";
-import GallerySection from "./components/gallerySection/GallerySection";
-import ContactCTA from "./components/contactCta/ContactCTA";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
-   const location = useLocation();
   return (
     <>
       <Header />
-      {location.pathname === "/" && (
-       <>
-       <Hero />
-       <ServicesSection/>
-       <AboutSection/>
-       <GallerySection />
-       <ContactCTA withForm={true} />
-       </>
-
-      )}
-
-
       <main>
         <Routes>
-          <Route path="/" element={<div></div>} />
-          <Route path="/om-oss" element={<div>Om oss</div>} />
-          <Route path="/tjanster" element={<div>Tjänster</div>} />
-          <Route path="/galleri" element={<div>Galleri</div>} />
-          <Route path="/kontakt" element={<div>Kontakt</div>} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
